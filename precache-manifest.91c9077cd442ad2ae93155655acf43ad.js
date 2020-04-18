f0159def51e6d056056bfd26f950e991d7d36776
issackj1 <issack.john1@ucalgary.ca>
2020-04-18 16:12:23 -0400

    Updates

---
+++ precache-manifest.91c9077cd442ad2ae93155655acf43ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4279be1fefa28022c886da6e0768dab",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "f1b0408871199b5730b5",
+    "url": "/react-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1d46604a4e87f64f01ee",
+    "url": "/react-site/static/js/2.d7d08f1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-site/static/js/2.d7d08f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b0408871199b5730b5",
+    "url": "/react-site/static/js/main.4628cb86.chunk.js"
+  },
+  {
+    "revision": "c6744c51816cb12b6e86",
+    "url": "/react-site/static/js/runtime-main.4f9f756c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
