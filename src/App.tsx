4d724b24894f65ed5322ea4cdbe4dedec6aa32ab
issackj1 <issack.john1@ucalgary.ca>
2020-06-20 13:58:08 -0400

    added more properties to hackathon pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import "./App.css";
 import {HashRouter as Router, Switch, Route} from "react-router-dom";
 import {Home} from "./components/Home";
-import {StarterHacks2020} from "./components/StarterHacks2020";
 import {Shuffl} from "./components/Shuffl";
 import {AstroJump} from "./components/AstroJump";
-import {NsbeHacks} from "./components/NsbeHacks";
 import {HackathonPage} from "./components/HackathonPage";
 import {HackathonOne, HackathonTwo, NoHackathon} from "./constants";
 
@@ -28,8 +26,6 @@ function App() {
     return (
         <Router basename={process.env.PUBLIC_URL}>
             <Switch>
-                <Route path="/starterhacks2020" component={StarterHacks2020}/>
-                <Route path="/nsbehacks2020" component={NsbeHacks}/>
                 <Route path="/shuffl" component={Shuffl}/>
                 <Route path="/astrojump" component={AstroJump}/>
                 <Route path="/hackathon/:name" render={(routeProps) => (
--- src/components/HackathonPage.tsx
+++ src/components/HackathonPage.tsx
@@ -10,28 +10,55 @@ interface Props {
     projectTitle: string,
     summary: string,
     howItWorks: string,
+    howWeBuilt: string,
+    challenges: string,
+    accomplishments: string,
+    learned: string,
+    next: string,
+    repoUrl: string,
+    technologies: string[],
 
 }
 
 export const HackathonPage: React.FC<Props> = (props) => {
-    const { name, year, date, location, projectTitle, summary, howItWorks} = props;
+    const {
+        name, year, date, location, projectTitle, summary, howItWorks,
+        howWeBuilt, challenges, accomplishments, learned, next, repoUrl, technologies
+    } = props;
     return (<div>
         <NavBar/>
         <Container>
             <Row>
                 <Col>
                     <h1>{name} {year}</h1>
-                    <h5>{date} {location}</h5>
-                    <br/>
-                    <br/>
+                    <h5 className="mb-5">{date} {location}</h5>
                     <h3>{projectTitle} Inspiration</h3>
                     <p>{summary}</p>
+
                     <h3>How it Works</h3>
                     <p>{howItWorks}</p>
+
+                    <h3>How we built it</h3>
+                    <p>{howWeBuilt}</p>
+
+                    <h3>Challenges we ran into</h3>
+                    <p>{challenges}</p>
+
+                    <h3>Accomplishments that we're proud of</h3>
+                    <p>{accomplishments}</p>
+
+                    <h3>What we learned</h3>
+                    <p>{learned}</p>
+
+                    <h3>What's next for {projectTitle}</h3>
+                    <p>{next}</p>
+                    <a href={repoUrl}
+                       rel={"noopener noreferrer"}
+                       target={"_blank"}>Repository</a>
+
                     <h3>Built with</h3>
                     <ListGroup horizontal>
-                        <ListGroup.Item>Django</ListGroup.Item>
-                        <ListGroup.Item>Python</ListGroup.Item>
+                        {technologies.map((name) => <ListGroup.Item>{name}</ListGroup.Item>)}
                     </ListGroup>
                 </Col>
             </Row>
