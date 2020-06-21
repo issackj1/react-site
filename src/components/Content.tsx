408b52488e8beda99b852319c5070e3c33b96a5a
issackj1 <issack.john1@ucalgary.ca>
2020-06-20 23:36:57 -0400

    added parallax scrolling

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,21 +6,33 @@ import {Education} from "./Education";
 import {Experience} from "./Experience";
 import {Extracurricular} from "./Extracurricular";
 import {Contact} from "./Contact";
-import {Container} from "react-bootstrap";
+import {ParallaxLayer} from "react-spring/renderprops-addons";
 
 interface Props {
 
 }
 
 export const Content: React.FC<Props> = () => {
-    return (
-        <Container>
-            <About/>
-            <Skills/>
-            <Education/>
-            <Experience/>
-            <Extracurricular/>
-            <Contact/>
-        </Container>
+
+    return (<>
+            <ParallaxLayer offset={1} speed={1}>
+                <About/>
+            </ParallaxLayer>
+            <ParallaxLayer offset={2} speed={0.8}>
+                <Skills/>
+            </ParallaxLayer>
+            <ParallaxLayer offset={3} speed={0.6}>
+                <Education/>
+            </ParallaxLayer>
+            <ParallaxLayer offset={4} speed={0.5}>
+                <Experience/>
+            </ParallaxLayer>
+            <ParallaxLayer offset={5} speed={0.4}>
+                <Extracurricular/>
+            </ParallaxLayer>
+            <ParallaxLayer offset={6} speed={0.3}>
+                <Contact/>
+            </ParallaxLayer>
+        </>
     );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,20 @@ import {NavBar} from "./NavBar";
 import logo from "../logo.svg";
 import {Content} from "./Content";
 import {useSpring, animated} from 'react-spring';
+import {Parallax, ParallaxLayer} from "react-spring/renderprops-addons";
+import {About} from "./About";
+import {Skills} from "./Skills";
+import {Education} from "./Education";
+import {Experience} from "./Experience";
+import {Extracurricular} from "./Extracurricular";
+import {Contact} from "./Contact";
 
 interface Props {
     dropDownLinks: link[],
     navLinks: link[]
 }
 
-interface link  {
+interface link {
     title: string,
     path: string
 }
@@ -35,22 +42,28 @@ export const Home: React.FC<Props> = (props) => {
     });
 
     // @ts-ignore
-    return (<animated.div scrollTop={springProps.scroll} className="App" style={springProps}>
+    let parallax;
+    // @ts-ignore
+    return (<div className="App">
         <NavBar  {...props} />
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-                Issack John
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learning React
-            </a>
-        </header>
-        <Content/>
-    </animated.div>)
+        <Parallax pages={7} scrolling={true} ref={ref => (parallax = ref)}>
+            <ParallaxLayer offset={0} speed={0.5}>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Issack John
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learning React
+                    </a>
+                </header>
+            </ParallaxLayer>
+            <Content/>
+        </Parallax>
+    </div>)
 };
