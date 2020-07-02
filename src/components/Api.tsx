7ebf23d100d8fa9e6d90ff176a0b0884ccff818f
issackj1 <issack.john1@ucalgary.ca>
2020-07-01 23:49:05 -0400

    auth when you land on api page

--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 import { ApiLogin } from "./ApiLogin";
 import { ApiHome } from "./ApiHome";
+
+const axios = require('axios');
 
 interface Props {
 
@@ -9,12 +11,27 @@ interface Props {
 
 export const Api: React.FC<Props> = () => {
 
-    const loggedIn = false;
+    const [isAuthenticated, setAuthentication] = useState(false);
+
+    useEffect(() => {
+        authenticate();
+    })
+
+    const authenticate = () => {
+        axios.post('http://3.94.8.68:4000/api/confirm-token/')
+            .then((result: any) => {
+                    setAuthentication(true)
+                },
+                (error: any) => {
+                    console.log('you need to log in bro')
+                }
+            );
+    }
 
     return (
         <div className={"container d-flex text-center justify-content-center flex-column h-100 m-auto"}>
             {
-                loggedIn
+                isAuthenticated
                     ? <ApiHome/>
                     : <ApiLogin/>
             }
