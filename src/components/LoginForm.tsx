8e25bfc44f6dfa7ec72b8c348f8d9d218d1fc022
issackj1 <issack.john1@ucalgary.ca>
2020-07-01 23:14:31 -0400

    masterial-ui integration for login page

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Form, Formik, useField } from "formik";
-import { Button } from "react-bootstrap";
+import { Field, Form, Formik } from "formik";
 import * as Yup from 'yup';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+import { Button } from "@material-ui/core";
 
 interface Props {
     handleSubmit: (username: string, password: string) => any
@@ -17,23 +18,6 @@ const schema = Yup.object({
         .required('Please enter a password')
 });
 
-// @ts-ignore
-const MyTextField = ({ label, ...props }) => {
-    // @ts-ignore
-    const [field, meta, helpers] = useField(props);
-    return (
-        <>
-            <label>
-                {label}
-                <input {...field} {...props} />
-            </label>
-            {meta.touched && meta.error ? (
-                <div className='error'>{meta.error}</div>
-            ) : null}
-        </>
-    );
-};
-
 export const LoginForm: React.FC<Props> = (props) => {
 
     return (
@@ -42,19 +26,19 @@ export const LoginForm: React.FC<Props> = (props) => {
             validationSchema={schema}
             onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
-                    // alert(JSON.stringify(values, null, 2));
                     props.handleSubmit(values.email, values.password);
                     setSubmitting(false);
                 }, 400);
             }}>
             {({ isSubmitting }) => (
                 <Form className={"d-flex flex-column"}>
-                    <MyTextField type="email" name="email" as={"input"} placeholder={"Email"} label={"Email"}/>
-                    <MyTextField type="password" name="password" as={"input"} placeholder={"Password"}
-                                 label={"password"}/>
-                    <Button type="submit" disabled={isSubmitting}>
-                        Submit
-                    </Button>
+                    <Field className={"mb-3"} component={TextField} name="email" label={"email"} variant="outlined"
+                           InputProps={{ notched: true }}/>
+                    <Field className={"mb-3"} component={TextField} name="password" label={"password"}
+                           variant="outlined" InputProps={{ notched: true }}/>
+                    <Field component={CheckboxWithLabel} name="checked"
+                           Label={{ label: 'Agree to terms and conditions' }}/>;
+                    <Button type={"submit"} variant="contained" disabled={isSubmitting} color="primary">Submit</Button>
                 </Form>
             )}
         </Formik>
