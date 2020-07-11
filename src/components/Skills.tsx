48cdf3e1b89ca7d10666d29d89546b0d3dcd30f9
issackj1 <issack.john1@ucalgary.ca>
2020-07-11 17:13:42 -0400

    styling

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import { Col, Row } from "react-bootstrap";
 import { SkillsContent } from "../constants";
+import { Typography } from "@material-ui/core";
 
 interface Props {
 
@@ -10,11 +11,19 @@ interface Props {
 export const Skills: React.FC<Props> = () => {
     return (
         <div className={"container"} id={"skills"}>
-            <h1>{SkillsContent.title}</h1>
-            <h5>{SkillsContent.subtitle}</h5>
             <Row>
                 <Col>
-                    <h3>Languages</h3>
+                    <Typography variant={"h2"}>{SkillsContent.title}</Typography>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Typography variant={"h5"}>{SkillsContent.subtitle}</Typography>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Typography variant={"h3"}>Languages</Typography>
                     <ul className={"list-unstyled"}>
                         {
                             SkillsContent.languages.map((language, i) => <li key={i}>{language}</li>)
@@ -22,7 +31,7 @@ export const Skills: React.FC<Props> = () => {
                     </ul>
                 </Col>
                 <Col>
-                    <h3>Libraries</h3>
+                    <Typography variant={"h3"}>Libraries</Typography>
                     <ul className={"list-unstyled"}>
                         {
                             SkillsContent.libraries.map((library, i) => <li key={i}>{library}</li>)
@@ -30,7 +39,7 @@ export const Skills: React.FC<Props> = () => {
                     </ul>
                 </Col>
                 <Col>
-                    <h3>Frameworks</h3>
+                    <Typography variant={"h3"}>Frameworks</Typography>
                     <ul className={"list-unstyled"}>
                         {
                             SkillsContent.frameworks.map((framework, i) => <li key={i}>{framework}</li>)
@@ -38,7 +47,7 @@ export const Skills: React.FC<Props> = () => {
                     </ul>
                 </Col>
                 <Col>
-                    <h3>Databases</h3>
+                    <Typography variant={"h3"}>Databases</Typography>
                     <ul className={"list-unstyled"}>
                         {
                             SkillsContent.databases.map((database, i) => <li key={i}>{database}</li>)
