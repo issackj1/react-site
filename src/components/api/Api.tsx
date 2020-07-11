36afc1d694ebd47e6a5a6ef072ddd6a04dbf7e78
issackj1 <issack.john1@ucalgary.ca>
2020-07-11 00:18:24 -0400

    Update Api.tsx

--- src/components/api/Api.tsx
+++ src/components/api/Api.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import { Auth } from "./Auth";
 import { Home } from "./Home";
+import { CircularProgress, Fade } from "@material-ui/core";
 
 const axios = require('axios');
 
@@ -12,36 +13,52 @@ interface Props {
 export const Api: React.FC<Props> = () => {
 
     const [authenticated, setAuthenticated] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
+        const authenticate = async () => {
+            setIsLoading(true)
+            setIsError(false)
+            await axios.post('http://localhost:4000/api/confirm-token', {},
+                {
+                    credentials: 'include',
+                    headers: {
+                        Authorization: localStorage.getItem('my-jwt')
+                    }
+                })
+                .then((result: any) => {
+                        setAuthenticated(true)
+                    },
+                    (error: any) => {
+                        setIsError(true)
+                    }
+                );
+            setIsLoading(false);
+        }
         authenticate();
     }, [authenticated])
 
-    const authenticate = () => {
-        axios.post('http://localhost:4000/api/confirm-token',
-            {},
-            {
-                credentials: 'include',
-                headers: {
-                    Authorization: localStorage.getItem('my-jwt')
-                }
-            })
-            .then((result: any) => {
-                    setAuthenticated(true)
-                },
-                (error: any) => {
-                }
-            );
-    }
 
     return (
         <div className={"container d-flex text-center justify-content-center flex-column h-100 m-auto"}>
-            {
-                authenticated
+            {isLoading
+                ? (
+                    <div>
+                        <Fade
+                            in={isLoading}
+                            style={{
+                                transitionDelay: isLoading ? '800ms' : '0ms',
+                            }}
+                            unmountOnExit
+                        >
+                            <CircularProgress/>
+                        </Fade>
+                    </div>
+                )
+                : authenticated
                     ? <Home/>
-                    : <Auth
-                        setAuthenticated={() => setAuthenticated(true)}
-                    />
+                    : <Auth setAuthenticated={() => setAuthenticated(true)}/>
             }
         </div>
     );
