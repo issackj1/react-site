c929bbbc01c19096eb5f7cc45921d2d5c7d46dcb
issackj1 <issack.john1@ucalgary.ca>
2020-08-30 16:16:21 -0600

    fixed skill page layout

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,46 +7,58 @@ interface Props {
 }
 
 export const Skills: React.FC<Props> = () => {
-    return (
-        <Grid direction="column"
-            spacing={2}
-            justify="center">
-            <Typography variant={"h2"} align={"center"} gutterBottom>{SkillsContent.title}</Typography>
-            <Typography variant={"h5"} align={"center"} gutterBottom>{SkillsContent.subtitle}</Typography>
-            <Grid direction="column" justify="center">
-                <Grid item xs={ 3 } md={ 3 }>
-                    <Typography variant={"h4"}>Languages</Typography>
-                    <ul className={"list-unstyled"}>
-                        {
-                            SkillsContent.languages.map((language, i) => <li key={i}>{language}</li>)
-                        }
-                    </ul>
-                </Grid>
-                <Grid item xs={ 3 } md={ 3 }>
-                    <Typography variant={"h4"}>Libraries</Typography>
-                    <ul className={"list-unstyled"}>
-                        {
-                            SkillsContent.libraries.map((library, i) => <li key={i}>{library}</li>)
-                        }
-                    </ul>
-                </Grid>
-                <Grid item xs={ 3 } md={ 3 }>
-                    <Typography variant={"h4"}>Frameworks</Typography>
-                    <ul className={"list-unstyled"}>
-                        {
-                            SkillsContent.frameworks.map((framework, i) => <li key={i}>{framework}</li>)
-                        }
-                    </ul>
-                </Grid>
-                <Grid item xs={ 3 } md={ 3 }>
-                    <Typography variant={"h4"}>Databases</Typography>
-                    <ul className={"list-unstyled"}>
-                        {
-                            SkillsContent.databases.map((database, i) => <li key={i}>{database}</li>)
-                        }
-                    </ul>
-                </Grid>
-            </Grid>
-        </Grid>
-    );
+	return (
+		<Grid
+			container
+			direction="row"
+			spacing={ 2 }
+			justify="center">
+			<Grid
+				item
+				xs={ 12 }
+				sm={ 12 }
+			>
+				<Typography variant={ "h2" } align={ "center" } gutterBottom>{ SkillsContent.title }</Typography>
+			</Grid>
+			<Grid
+				item
+				xs={ 12 }
+				sm={ 12 }
+			>
+				<Typography variant={ "h5" } align={ "center" } gutterBottom>{ SkillsContent.subtitle }</Typography>
+			</Grid>
+			<Grid item xs={ 12 } sm={ 3 }>
+				<Typography variant={ "h4" }>Languages</Typography>
+				<ul className={ "list-unstyled" }>
+					{
+						SkillsContent.languages.map((language, i) => <li key={ i }>{ language }</li>)
+					}
+				</ul>
+			</Grid>
+			<Grid item xs={ 12 } sm={ 3 }>
+				<Typography variant={ "h4" }>Libraries</Typography>
+				<ul className={ "list-unstyled" }>
+					{
+						SkillsContent.libraries.map((library, i) => <li key={ i }>{ library }</li>)
+					}
+				</ul>
+			</Grid>
+			<Grid item xs={ 12 } sm={ 3 }>
+				<Typography variant={ "h4" }>Frameworks</Typography>
+				<ul className={ "list-unstyled" }>
+					{
+						SkillsContent.frameworks.map((framework, i) => <li key={ i }>{ framework }</li>)
+					}
+				</ul>
+			</Grid>
+			<Grid item xs={ 12 } sm={ 3 }>
+				<Typography variant={ "h4" }>Databases</Typography>
+				<ul className={ "list-unstyled" }>
+					{
+						SkillsContent.databases.map((database, i) => <li key={ i }>{ database }</li>)
+					}
+				</ul>
+			</Grid>
+		</Grid>
+	);
 };
