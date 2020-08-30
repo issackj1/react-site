073e32baf0fe7e795e09944dfbac8b2da781e728
issackj1 <issack.john1@ucalgary.ca>
2020-08-30 16:36:12 -0600

    fixed contact layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Grid, Typography } from "@material-ui/core";
 
 interface Props {
 
 }
 
-export const Contact:React.FC<Props> = () => {
-    return (
-        <div className={"container"} id={"contact"}>
-            <h1>GET IN TOUCH</h1>
-            <h5>Contact</h5>
-        </div>
-    );
+export const Contact: React.FC<Props> = () => {
+	return (
+		<Grid
+			container
+			direction="row"
+			spacing={ 2 }>
+			<Grid item xs={ 12 } sm={ 12 }>
+				<Typography variant={ "h2" } align={ "center" }>GET IN TOUCH</Typography>
+			</Grid>
+			<Grid item xs={ 12 } sm={ 12 }>
+				<Typography variant={ "h5" } align={ "center" }>Contact</Typography>
+			</Grid>
+		</Grid>
+	);
 };
