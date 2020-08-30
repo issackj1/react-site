6bf5e9fd07c5f5a2ef68c28c1f3b809a86b60dbe
issackj1 <issack.john1@ucalgary.ca>
2020-08-30 16:22:53 -0600

    fixed education page layout

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import { Grid, Typography } from "@material-ui/core";
 
 interface Props {
 
 }
 
 export const Education: React.FC<Props> = () => {
-    return (
-        <div className={"container"} id={"education"}>
-            <h1>EDUCATION</h1>
-            <h5>Education</h5>
-            <ul className={"list-unstyled"}>
-                <li>Bachelor of Science Major in Computer Science</li>
-            </ul>
-        </div>
-    );
+	return (
+		<Grid
+			container
+			direction={ "row" }
+			justify={"center"}
+		>
+			<Grid item
+			      xs={ 12 }
+			      sm={ 12 }>
+
+				<Typography variant={ "h2" } align={"center"}>
+					EDUCATION
+				</Typography>
+			</Grid>
+			<Grid item
+			      xs={ 12 }
+			      sm={ 12 }>
+				<Typography variant={ "h5" } align={"center"}>
+					Education
+				</Typography>
+			</Grid>
+			<Grid item xs={ 12 } sm={ 12 }>
+				<Typography variant={ "body1" } align={"center"}>
+					Bachelor of Science Major in Computer Science
+				</Typography>
+			</Grid>
+		</Grid>
+	);
 };
