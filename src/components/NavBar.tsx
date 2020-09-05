760e0efdba6ecb04a05f26d79927f5796d637f69
issackj1 <issack.john1@ucalgary.ca>
2020-09-05 14:17:40 -0600

    Update NavBar.tsx
    Made navbar more dense and switched nav items from links to buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,9 @@
 // @ts-nocheck
 import React from 'react';
-import { AppBar, Link, Toolbar, Typography, } from "@material-ui/core";
+import { AppBar, Button, Link, Toolbar, Typography, } from "@material-ui/core";
 import { Parallax } from "react-spring/renderprops-addons";
 import { navProps } from "../constants";
 import { makeStyles } from "@material-ui/core/styles";
-import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
-
 
 interface Props {
 	parallax: Parallax
@@ -32,37 +29,22 @@ export const NavBar: React.FC<Props> = ({ parallax }) => {
 
 	const { dropDownLinks, navLinks } = navProps;
 	const [auth, setAuth] = React.useState(false);
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const open = Boolean(anchorEl);
 	const classes = useStyles();
-
-	const handleMenu = (event: { currentTarget: React.SetStateAction<null>; }) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
 
 	return (
 		<div className={ classes.root }>
 			<AppBar position="static">
-				<Toolbar>
-					<IconButton edge="start" className={ classes.menuButton } color="inherit" aria-label="menu">
-						<MenuIcon/>
-					</IconButton>
+				<Toolbar variant={"dense"}>
 					<Typography variant="h6" className={ classes.title }>
 						<Link underline={ "none" } color={ "inherit" } href={ "/" }>
 							Issack John
 						</Link>
 					</Typography>
-					<Typography className={ classes.root }>
-						{ navLinks.map(({ title }, i) => <Link underline={ "none" } color={ "inherit" } href={ "#" }
-						                                       key={ i }
-						                                       onClick={ () => parallax.scrollTo(i + 1) }>{ title }</Link>
-						) }
-						<Link underline={ "none" } href={ "/api" } color={ "inherit" }>API</Link>
-					</Typography>
+
+					{ navLinks.map(({ title }, i) => <Button onClick={ () => parallax.scrollTo(i + 1) } key={ i }
+					                                        color="inherit">{ title }</Button>
+					) }
+					<Button color={"inherit"} href={"/api"}>API</Button>
 					{/*{ dropDownLinks.map(({ title, path }, i) =>*/ }
 					{/*	<Link key={ i } color="inherit" href={ path }>{ title }</Link>) }*/ }
 
