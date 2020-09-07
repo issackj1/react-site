f64b9e3baabcf4d152eae2fe0226135a3a8d66df
issackj1 <issack.john1@ucalgary.ca>
2020-09-07 15:05:39 -0600

    added links to some links within the Whimsical

--- src/components/financialLiteracy/Whimsical.tsx
+++ src/components/financialLiteracy/Whimsical.tsx
@@ -11,11 +11,31 @@ export const Whimsical: React.FC<Props> = (props) => {
 	return (
 		<Container maxWidth="lg">
 			<Box my={ 4 }>
-				<Typography color={ "textPrimary" } component={ "h6" } variant={ "h2" } align={ "center" }>Financial Literacy</Typography>
+				<Typography color={ "textPrimary" } variant={ "h3" } align={ "center" }>Financial Literacy</Typography>
 				<Grid container
-				direction={"row"} justify={"center"}>
+				      direction={ "row" }
+				spacing={2}>
+					<Grid container item direction={"column"} xs={6}>
+						<Typography color={ "textPrimary" } variant={ "h6" }>Fintech Apps</Typography>
+						<ul className={"list-unstyled"}>
+							<li><a href={"https://wealthsimple.com/invite/OBBTMW"}>Wealth Simple</a></li>
+							<li><a href={"https://www.questrade.com/home"}>Questrade</a></li>
+							<li><a href={"https://go.moka.ai/xk27G51pg8"}>Moka</a></li>
+							<li><a href={"https://www.webull.com/"}>Webull</a></li>
+						</ul>
+					</Grid>
+					<Grid container item direction={"column"} xs={6}>
+						<Typography color={ "textPrimary" } variant={ "h6" }>Newsletters</Typography>
+						<ul className={"list-unstyled"}>
+							<li><a href={"http://morningbrew.com/daily/r/?kid=173fbe8e"}>MORNING BREW</a></li>
+							<li><a href={"http://morningbrew.com/emerging-tech/r/?kid=173fbe8e"}>EMERGING TECH BREW</a></li>
+						</ul>
+					</Grid>
+				</Grid>
+				<Grid container
+				      direction={ "row" } justify={ "center" }>
 					<Iframe url="https://whimsical.com/embed/337yB3814c7Fs1JVmmqQGU"
-					        width={"1600px"}
+					        width={ "1600px" }
 					        height="500px"
 					        id="myId"
 					        className="myClassname"
