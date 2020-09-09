d4eca4a8a90c2f11c62bfa5c6bac4c204f8cf6e8
issackj1 <issack.john1@ucalgary.ca>
2020-09-08 22:00:16 -0600

    removed bootstrap use from project

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from '@material-ui/core/styles';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'fontsource-roboto';
 import App from './App';
 import theme from './theme';
