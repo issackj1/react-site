72d0302718a42953d438f8815acaf1173a4998b3
issackj1 <issack.john1@ucalgary.ca>
2020-09-08 21:26:57 -0600

    cleaned up constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,19 +6,4 @@ export const NoProject = require('./json/NoProject');
 export const AboutContent = require('./json/About.json');
 export const SkillsContent = require('./json/Skills.json');
 export const ExtracurricularContent = require('./json/Extracurricular.json');
-export const navProps = {
-    dropDownLinks: [
-        { title: 'Starter Hacks 2020', path: '/project/starterhacks2020' },
-        { title: 'NSBE Hacks 2020', path: '/project/nsbehacks2020' },
-        { title: 'Shuffl', path: '/project/shuffl' },
-        { title: 'Astro Jump', path: '/project/astrojump' },
-    ],
-    navLinks: [
-        { title: 'About' },
-        { title: 'Skills' },
-        { title: 'Education' },
-        { title: 'Experience' },
-        { title: 'Extracurricular' },
-        { title: 'Contact' },
-    ]
-};
+export const navProps = require("./json/NavProps.json");
