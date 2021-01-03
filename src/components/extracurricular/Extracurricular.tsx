ef010aa3100ea6f831f5291c10300a4bfd799143
issackj1 <issack.john1@ucalgary.ca>
2021-01-02 20:50:23 -0700

    removing duplicate code in extracurricular

--- src/components/extracurricular/Extracurricular.tsx
+++ src/components/extracurricular/Extracurricular.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { ExtracurricularContent } from "../../constants";
-import { Grid, Typography } from "@material-ui/core";
+import {ExtracurricularContent} from "../../constants";
+import {Grid, Typography} from "@material-ui/core";
 import {ExtracurricularItem} from "./ExtracurricularItem";
+import {v4 as uuidv4} from "uuid";
 
 interface Props {
 
 }
 
-interface Book {
-	name : String
-	author: String
+interface ExtracurricularContentItems {
+    title: String
+    items: []
 }
 
 export const Extracurricular: React.FC<Props> = () => {
-	return (
-		<div className={ "container" } id={ "extracurricular" }>
-			<Grid container direction={ "row" } spacing={ 1 }>
-				<Grid item xs={ 12 } sm={ 12 }>
-					<Typography variant={ "h2" } gutterBottom
-					            align={ "center" }>{ ExtracurricularContent.title }</Typography>
-				</Grid>
-				<ExtracurricularItem title={"Hackathons Attended"} items={ExtracurricularContent.hackathons}/>
-				<ExtracurricularItem title={"Favourite Books"} items={ExtracurricularContent.books}/>
-			</Grid>
-		</div>
-	);
+    return (
+        <div className={"container"} id={"extracurricular"}>
+            <Grid container direction={"row"} spacing={1}>
+                <Grid item xs={12} sm={12}>
+                    <Typography variant={"h2"} gutterBottom
+                                align={"center"}>{ExtracurricularContent.title}</Typography>
+                </Grid>
+                {
+                    ExtracurricularContent.content.map((object: ExtracurricularContentItems) => <ExtracurricularItem
+                        key={uuidv4()} title={object.title} items={object.items}/>)
+                }
+            </Grid>
+        </div>
+    );
 };
