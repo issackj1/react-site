2ed8a87ba6f7af3b174751ca994ce3eae778a911
issackj1 <issack.john1@ucalgary.ca>
2023-03-05 16:48:32 -0800

    Update Whimsical.tsx

--- src/components/financialLiteracy/Whimsical.tsx
+++ src/components/financialLiteracy/Whimsical.tsx
@@ -104,7 +104,6 @@ export const Whimsical: React.FC<Props> = () => {
               </ListItemLink>
             ))}
           </List>
-          ``
         </Collapse>
       </List>
       <Iframe
