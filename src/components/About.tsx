8f8549bdc1a7e89c5bd312301c7d7f96b758514e
issackj1 <issack.john1@ucalgary.ca>
2023-04-05 22:41:52 -0700

    change layout of pfp and about description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,19 +7,31 @@ interface Props {}
 
 export const About: React.FC<Props> = () => {
   return (
-    <Grid container direction={"column"} spacing={3}>
-      <Typography variant={"h2"} align={"center"}>
-        {AboutContent.title}
-      </Typography>
-      <Avatar
-        variant={"rounded"}
-        alt="Issack John"
-        src={profilePicture}
-        sx={{ width: "25rem", height: "20rem", margin: "auto" }}
-      />
-      <Typography variant={"body1"} align={"center"}>
-        {AboutContent.summary}
-      </Typography>
+    <Grid
+      container
+      direction={"row"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      spacing={2}
+    >
+      <Grid item sm={12}>
+        <Typography variant={"h2"} align={"center"}>
+          {AboutContent.title}
+        </Typography>
+      </Grid>
+      <Grid item sm={4}>
+        <Avatar
+          variant={"rounded"}
+          alt="Issack John"
+          src={profilePicture}
+          sx={{ width: "20rem", height: "15rem", margin: "auto" }}
+        />
+      </Grid>
+      <Grid item sm={4}>
+        <Typography variant={"body1"} align={"center"}>
+          {AboutContent.summary}
+        </Typography>
+      </Grid>
     </Grid>
   );
 };
